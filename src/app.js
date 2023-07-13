8c0dcfabb3b1219116db27dcf6e850ed5df18342
Tsaone <tsaoneb3@gmail.com>
2023-07-13 16:39:22 +0200

    moved HTML forecast to JS

--- src/app.js
+++ src/app.js
@@ -22,6 +22,34 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function displayForecast(){
+  let forecastElement = document.querySelector("#forecast");
+
+  let forecastHTML = `<div class="row">`;
+  let days = ["Thu", "Fri", "Sat", "Sun", "Mon", "Tue"];
+  days.forEach(function (day) {
+  forecastHTML = forecastHTML + 
+    `
+     <div class="col-2">
+       <div class="weather-forecast-date">${day}</div>
+         <img
+          src="http://openweathermap.org/img/wn/02d@2x.png"
+          alt=""
+          width="40"
+        />
+        <div class="weather-forecast-temperatures">
+          <span class="weather-forecast-temperature-max">18°</span>
+          <span class="weather-forecast-temperature-min">12°</span>
+        </div>
+      </div>
+
+  `;
+  })
+
+  forecastHTML = forecastHTML + `</div>`;
+  forecastElement.innerHTML = forecastHTML;
+}
+
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
@@ -88,3 +116,4 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("Gaborone");
+displayForecast();
